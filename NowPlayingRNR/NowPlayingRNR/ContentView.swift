cecab2c0b0a8267dc56c358d027dd8d6ba20f87c
Robert Redmond <robert_redmond@yahoo.com>
2025-07-28 16:28:27 +0100

    Iniitail Xcode

---
+++ NowPlayingRNR/NowPlayingRNR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NowPlayingRNR
+//
+//  Created by Robert Redmond on 28/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NowPlayingRNR/NowPlayingRNR/NowPlayingRNRApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingRNRApp.swift
+//  NowPlayingRNR
+//
+//  Created by Robert Redmond on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct NowPlayingRNRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
